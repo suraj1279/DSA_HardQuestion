79b0d22f048d09e99c06beb1003fd99190260e52
Suraj <147306805+suraj1279@users.noreply.github.com>
2024-08-09 23:21:19 +0530

    Burning Tree.java
    Day 1
    Question 3

---
+++ Burning Tree.java
@@ -0,0 +1,181 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
+
+class GfG {
+
+	static Node buildTree(String str) {
+
+		if (str.length() == 0 || str.charAt(0) == 'N') {
+			return null;
+		}
+
+		String ip[] = str.split(" ");
+		// Create the root of the tree
+		Node root = new Node(Integer.parseInt(ip[0]));
+		// Push the root to the queue
+
+		Queue<Node> queue = new LinkedList<>();
+
+		queue.add(root);
+		// Starting from the second element
+
+		int i = 1;
+		while (queue.size() > 0 && i < ip.length) {
+
+			// Get and remove the front of the queue
+			Node currNode = queue.peek();
+			queue.remove();
+
+			// Get the current node's value from the string
+			String currVal = ip[i];
+
+			// If the left child is not null
+			if (!currVal.equals("N")) {
+
+				// Create the left child for the current node
+				currNode.left = new Node(Integer.parseInt(currVal));
+				// Push it to the queue
+				queue.add(currNode.left);
+			}
+
+			// For the right child
+			i++;
+			if (i >= ip.length)
+				break;
+
+			currVal = ip[i];
+
+			// If the right child is not null
+			if (!currVal.equals("N")) {
+
+				// Create the right child for the current node
+				currNode.right = new Node(Integer.parseInt(currVal));
+
+				// Push it to the queue
+				queue.add(currNode.right);
+			}
+			i++;
+		}
+
+		return root;
+	}
+
+	static void printInorder(Node root) {
+		if (root == null)
+			return;
+
+		printInorder(root.left);
+		System.out.print(root.data + " ");
+
+		printInorder(root.right);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		while (t > 0) {
+			String s = br.readLine();
+			int target = Integer.parseInt(br.readLine());
+			Node root = buildTree(s);
+
+			Solution g = new Solution();
+			System.out.println(g.minTime(root, target));
+			t--;
+
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    /*class Node {
+    	int data;
+    	Node left;
+    	Node right;
+    
+    	Node(int data) {
+    		this.data = data;
+    		left = null;
+    		right = null;
+    	}
+    }*/
+
+
+
+  my code----------------------------------------------------------------------------------------
+    private static int Burn(Node root, int target, int [] timer){
+        if(root==null){
+            return 0;
+        }
+        if(root.data==target){
+            return -1;
+        }
+        int left=Burn(root.left,target,timer);
+        int right=Burn(root.right,target,timer);
+        if(left<0){
+            timer[0]=Math.max(timer[0],Math.abs(left)+right);
+            return left-1;
+        }
+        if(right<0){
+            timer[0]=Math.max(timer[0],Math.abs(right)+left);
+            return right-1;
+        }
+        return 1+Math.max(left,right);
+        
+    }
+    private static Node findBurn(Node root,int target){
+        if(root==null){
+            return null;
+        }
+        if(root.data==target){
+            return root;
+        }
+        Node left=findBurn(root.left,target);
+        if(left!=null) return left;
+        return findBurn(root.right,target);
+    }
+    private static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+    public static int minTime(Node root, int target) 
+    {
+        // Your code goes here
+        int []timer=new int[1];
+        timer[0]=0;
+         Burn(root,target,timer);
+        Node node=null;
+        node= findBurn(root,target);
+         int high=  height(node)-1;
+        return Math.max(timer[0],high);
+    }
+}
